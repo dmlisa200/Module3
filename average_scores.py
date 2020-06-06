7728932617439242eebadcb3d0de6bf107626ccd
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2020-06-06 10:33:06 -0500

    Add files via upload

---
+++ average_scores.py
@@ -0,0 +1,8 @@
+def average():
+    score1 = input("Enter score1: ")
+    score2 = input("Enter score2: ")
+    score3 = input("Enter score3: ")
+    return (int(score1) + int(score2) + int(score3))/3
+
+
+if __name__ == '__main__':
---
+++ test_average_scores.py
@@ -0,0 +1,11 @@
+import unittest
+from format_output import average_scores
+
+class MyTestCase(unittest.TestCase):
+    def test_average(self):
+        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
+            assert topic2.average() == 90
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
